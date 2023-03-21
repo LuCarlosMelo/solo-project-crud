ae92268e7a7c39b4e49f94c65d040e716f19a438
LuCarlosMelo <Luizcapeme@gmail.com>
2023-03-21 19:51:06 -0300

    Pagination

--- backend/src/main/java/com/lucarlosmelo/crud/ClientResource.java
+++ backend/src/main/java/com/lucarlosmelo/crud/ClientResource.java
@@ -1,17 +1,19 @@
 package com.lucarlosmelo.crud;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.lucarlosmelo.crud.entities.dto.ClientDTO;
 import com.lucarlosmelo.crud.services.ClientService;
 
-@Controller
+@RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 
@@ -19,9 +21,14 @@ public class ClientResource {
 	private ClientService service;
 
 	@GetMapping
-	public ResponseEntity<List<ClientDTO>> findAll() {
-		List<ClientDTO> dto = service.findAll();
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);		
+		Page<ClientDTO> dto = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok(dto);
 	}
-
+		
 }
