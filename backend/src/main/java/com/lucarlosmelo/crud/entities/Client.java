ee7fbabc561a704a1eeb2f0e970f8d156c410582
LuCarlosMelo <Luizcapeme@gmail.com>
2023-03-22 12:44:44 -0300

    timezone UTC

--- backend/src/main/java/com/lucarlosmelo/crud/entities/Client.java
+++ backend/src/main/java/com/lucarlosmelo/crud/entities/Client.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,14 +16,15 @@ import javax.persistence.Table;
 public class Client implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
 	private String cpf;
 	private Double income;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
 	private Integer children;
 
@@ -38,7 +40,7 @@ public class Client implements Serializable {
 		this.birthDate = birthDate;
 		this.children = children;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
@@ -58,11 +60,11 @@ public class Client implements Serializable {
 	public String getCpf() {
 		return cpf;
 	}
-	
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-	
+
 	public Double getIncome() {
 		return income;
 	}
@@ -74,7 +76,7 @@ public class Client implements Serializable {
 	public Instant getBirthDate() {
 		return birthDate;
 	}
-	
+
 	public void setBirthDate(Instant birthDate) {
 		this.birthDate = birthDate;
 	}
@@ -103,6 +105,5 @@ public class Client implements Serializable {
 		Client other = (Client) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
